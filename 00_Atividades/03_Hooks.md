0e6e89f628e200d67bcce1713333b4602f55b84e
Rafael Kasper <prof.rafaelkasper@gmail.com>
2024-08-30 20:29:15 -0300

    Created lesson 03

---
+++ 00_Atividades/03_Hooks.md
@@ -0,0 +1,13 @@
+# Atividade sobre Hooks -> useState e useEffect
+
+- Criar uma tela de login com styled-components, useState e useEffect
+
+  - A tela deve ter inputs de usuário e de senha e botão de confirmação (login/entrar)
+  - Guarde os valores do usuário e da senha usando o useState
+  - Crie um useEffect sem dependência e exiba um alerta na tela dizendo que o usuário não está logado
+  - No onPress do botão dispare um alerta mostrando os dados que o usuário digitou
+
+- Criar o projeto com `npx create-expo-app --template`
+- Instalar styled-components -> `npm install styled-components`
+- (Opcional) - Instalar uma fonte (à sua escolha) -> ver exemplo na aula 03_Hooks
+  - npx expo install expo-font @expo-google-fonts/inter
---
+++ 03_hooks/App.tsx
@@ -0,0 +1,96 @@
+import {
+  FiraCode_400Regular,
+  FiraCode_600SemiBold,
+  FiraCode_700Bold,
+} from "@expo-google-fonts/fira-code";
+import { useFonts } from "expo-font";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+export default function App() {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [teste, setTeste] = useState<string>("Não digitado");
+
+  useFonts({
+    FiraCode_400Regular,
+    FiraCode_600SemiBold,
+    FiraCode_700Bold,
+  });
+
+  const showLogedAlert = () => {
+    Alert.alert("Atenção!!!", `Nome: ${username}`, [
+      { text: "Legal!!!", onPress: () => console.log("apertou o botão") },
+    ]);
+  };
+
+  useEffect(() => {
+    if (password && password.length > 0) setTeste("digitado");
+  }, [password]);
+
+  useEffect(() => {
+    Alert.alert("Atenção!!!", "Bem vindo ao nosso super app", [
+      { text: "Legal!!!", onPress: () => console.log("apertou o botão") },
+    ]);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontSize: 26,
+          marginLeft: 10,
+          fontFamily: "FiraCode_400Regular",
+        }}
+      >
+        Nome de usuário
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+      />
+      <Text
+        style={{
+          fontSize: 26,
+          marginLeft: 10,
+          fontFamily: "FiraCode_600SemiBold",
+        }}
+      >
+        Senha
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <Text
+        style={{ fontSize: 26, marginLeft: 10, fontFamily: "FiraCode_700Bold" }}
+      >
+        {teste}
+      </Text>
+      <TouchableOpacity onPress={showLogedAlert}>
+        <Text>Logar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 80,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
