1a69bd84b4727aba39ee2b9401b8df1d81aeb453
Rafael Kasper <prof.rafaelkasper@gmail.com>
2024-10-25 20:06:36 -0300

    💫 Add animations

--- Atividades/06_carrinho-compras/src/screens/UserDetails.tsx
+++ Atividades/06_carrinho-compras/src/screens/UserDetails.tsx
@@ -1,25 +1,34 @@
 import { StyleSheet, Text, View, Button, Image } from "react-native";
 import React from "react";
 import { useUserContext } from "../contexts/UserContext";
-import Animated, { BounceInRight } from "react-native-reanimated";
+import Animated, {
+  BounceInDown,
+  BounceInLeft,
+  BounceInRight,
+  FlipInEasyX,
+} from "react-native-reanimated";
 
 const UserDetails = () => {
   const { userData, handleLogout } = useUserContext();
 
   return (
-    <Animated.View entering={BounceInRight}>
-      <Image
-        style={{ width: 100, height: 100 }}
-        source={{
-          uri: userData?.user.avatar,
-        }}
-      />
-      <Text>
-        {userData?.user.fname ?? ""} {userData?.user.lname ?? ""}
-      </Text>
-      <Text>{userData?.user.email ?? ""}</Text>
+    <View>
+      <Animated.View entering={FlipInEasyX.delay(150).duration(2000)}>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{
+            uri: userData?.user.avatar,
+          }}
+        />
+      </Animated.View>
+      <Animated.View entering={BounceInDown.delay(150).duration(2000)}>
+        <Text>
+          {userData?.user.fname ?? ""} {userData?.user.lname ?? ""}
+        </Text>
+        <Text>{userData?.user.email ?? ""}</Text>
+      </Animated.View>
       <Button onPress={handleLogout} title="Sair" />
-    </Animated.View>
+    </View>
   );
 };
 
